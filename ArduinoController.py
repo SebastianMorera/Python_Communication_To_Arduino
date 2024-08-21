27d4b3711c77a6c05ee2c451c6b1b16632fa9cd9
Sebastian Morera <sebastianmorera@Sebastians-MBP.hitronhub.home>
2024-08-20 22:53:20 -0400

    Modified ArduinoController class for better python standards and handle more exceptions

--- ArduinoController.py
+++ ArduinoController.py
@@ -1,7 +1,7 @@
 import serial
 
 class ArduinoController:
-    def __init__(self, device_name, protocol, baudrate, port, board_type= "Uno", timeout=1):
+    def __init__(self, device_name: str, protocol: str, baudrate: int, port: str, board_type= "Uno", timeout=1):
         self.device_name = device_name
         self.board_type = board_type
         self.protocol = protocol
@@ -11,29 +11,36 @@ class ArduinoController:
         self.connected = False
         self.serial_connection = None
 
-    def connect(self):
+    def connect(self) -> bool:
         try:
             self.serial_connection = serial.Serial(port=self.port, baudrate=self.baudrate, timeout=self.timeout)
             self.connected = True
-            print(f"Connection established with {self.device_name} at port {self.port} and baudrate of {self.baudrate}")
+            return True
         except serial.SerialException as e:
             self.connected = False
             print(f"Error: Connection could not be established. Error: {str(e)}")
+            return False
 
-    def disconnect(self):
+    def disconnect(self) -> None:
         if self.connected:
             self.serial_connection.close()
             self.connected = False
             print(f"Connection successfully closed with {self.device_name}")
 
-    def turn_on_led(self):
-        if self.connected:
-            self.serial_connection.write(b'O')
-        else:
+    def turn_on_led(self) -> None:
+        if not self.connected:
             print(f"Error: Not connected to {self.device_name}")
 
-    def turn_off_lef(self):
-        if self.connected:
-            self.serial_connection.write(b'F')
-        else:
+        try:
+            self.serial_connection.write(b'O')
+        except serial.SerialException as e:
+            print(f"Error: Failed to write to serial port: {str(e)}")
+
+    def turn_off_lef(self) -> None:
+        if not self.connected:
             print(f"Error: Not connected to {self.device_name}")
+
+        try:
+            self.serial_connection.write(b'F')
+        except serial.SerialException as e:
+            print(f"Error: Failed to write to serial port: {str(e)}")
--- main.py
+++ main.py
@@ -9,24 +9,25 @@ def SerialCommunicator():
         baudrate=9600,
         port='/dev/cu.usbmodem14101')
 
-    arduino.connect()
+    if arduino.connect():
+        print(f"Connection established with {arduino.device_name} at port {arduino.port} and baudrate of {arduino.baudrate}")
 
-    continue_loop = True
-    while continue_loop:
-        print("Enter 'O' to turn the led on, 'F' to turn the led off, 'Q' to quit")
-        user_selection = input().upper()
+        continue_loop = True
+        while continue_loop:
+            print("Enter 'O' to turn the led on, 'F' to turn the led off, 'Q' to quit")
+            user_selection = input().upper()
 
-        match user_selection:
-            case 'O':
-                arduino.turn_on_led()
-            case 'F':
-                arduino.turn_off_lef()
-            case 'Q':
-                continue_loop = False
-            case __:
-                print("Wrong selection entered, please try again.")
+            match user_selection:
+                case 'O':
+                    arduino.turn_on_led()
+                case 'F':
+                    arduino.turn_off_lef()
+                case 'Q':
+                    continue_loop = False
+                case __:
+                    print("Wrong selection entered, please try again.")
 
-    arduino.disconnect()
+        arduino.disconnect() if arduino.connected else print(f"{arduino.device_name} was not connected")
 
 
 if __name__ == "__main__":
